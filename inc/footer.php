c10009778befe4fd62e52faec03ec3d08ebf8eee
TNT535 <62098728+TNT535@users.noreply.github.com>
2022-05-29 16:17:07 +0700

    Update footer.

--- inc/footer.php
+++ inc/footer.php
@@ -1,59 +1,89 @@
+<!--footer starts from here-->
+<footer class="footer">
+<div class="container bottom_border">
+<div class="row">
+<div class=" col-sm-4 col-md col-sm-4  col-12 col">
+<h5 class="headin5_amrc col_white_amrc pt2">Find us</h5>
+<!--headin5_amrc-->
+<!-- <p class="mb10">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p> -->
+<p><i class="fa fa-building">Công ty Cổ phần Watch Store</i></p>
+<p><i class="fa fa-location-arrow"></i> Store 1: Khu phố 6, phường Linh Trung, Thủ Đức, TP.Hồ Chí Minh </p>
+<p><i class="fa fa-location-arrow"></i> Store 2:  90 Lê Văn Sỹ, P11, Phú Nhuận, TP.Hồ Chí Minh </p>
+
+
+
 </div>
-   <div class="footer">
-   	  <div class="wrapper">	
-	     <div class="section group">
-				<div class="col_1_of_4 span_1_of_4">
-						<h4>Thông tin</h4>
-						<ul>
-							<li>Công ty Cổ Phần Bookstore</li>
-							<li>Địa chỉ đăng ký kinh doanh: 29/1, đường số 4, KP.3, P. Bình Khánh, Q.2, TPHCM, Việt Nam</li>
-							<li>Mã số doanh nghiệp: 0106773786 do Sở Kế hoạch & Đầu tư TP Hồ Chí Minh cấp lần đầu ngày 10/02/2015</li>
-							<li>- Văn phòng chính: Tòa nhà Viettel, 285 Cách Mạng Tháng 8, Phường 12, Quận 10, Thành phố Hồ Chí Minh. <br>- Văn phòng: 52 Út Tịch, Phường 4, Quận Tân Bình, Thành Phố Hồ Chí Minh.</li>
-						</ul>
-					</div>
-				<div class="col_1_of_4 span_1_of_4">
-					<h4>Về Bookstore</h4>
-						<ul>
-							<li><a href="about.php">Giới thiệu Bookstore</a></li>
-							<li><a href="contact.php">Điều khoản sử dụng</a></li>
-							<li><a href="contact">Chính sách bảo mật</a></li>
-							<li><a href="contact.php"><span>Chính sách giải quyết khiếu nại</span></a></li>
-							<li><a href="contact.php"><span>Chính sách bảo mật thanh toán</span></a></li>
-						</ul>
-				</div>
-				<div class="col_1_of_4 span_1_of_4">
-					<h4>Tài khoản của tôi</h4>
-						<ul>
-							<li><a href="login.php">Đăng kí</a></li>
-							<li><a href="cart.php">Xem giỏ hàng</a></li>
-							<li><a href="wishlist.php">Sản phẩm yêu thích</a></li>
-							<li><a href="profile.php">Chi tiết tài khoản</a></li>
-							<li><a href="contact.php">Liên hệ</a></li>
-						</ul>
-				</div>
-				<div class="col_1_of_4 span_1_of_4">
-					<h4>Hỗ trợ</h4>
-						<ul>
-							<li>Tổng đài hỗ trợ: 19001221</li>
-							<li>Đối tác có nhu cầu hợp tác quảng cáo hoặc kinh doanh: marketing@bookstore.vn</li>
-						</ul>
-						<div class="social-icons">
-							<h4>KẾT NỐI VỚI CHÚNG TÔI</h4>
-					   		  <ul>
-							      <li class="facebook"><a href="#" target="_blank"> </a></li>
-							      <li class="twitter"><a href="#" target="_blank"> </a></li>
-							      <li class="googleplus"><a href="#" target="_blank"> </a></li>
-							      <li class="contact"><a href="#" target="_blank"> </a></li>
-							      <div class="clear"></div>
-						     </ul>
-   	 					</div>
-				</div>
-			</div>
-			<div class="copy_right">
-				<p>© 2021 Bookstore. Tất cả các quyền được bảo lưu. </p>
-		   </div>
-     </div>
-    </div>
+
+
+<div class=" col-sm-4 col-md  col-6 col">
+<h5 class="headin5_amrc col_white_amrc pt2">VỀ WATCH STORE</h5>
+<!--headin5_amrc-->
+<ul class="footer_ul_amrc">
+<li><a href="about.php">Giới thiệu Watch Store</a></li>
+<li><a href="contact.php">Điều khoản sử dụng</a></li>
+<li><a href="contact.php">Chính sách bảo mật</a></li>
+<li><a href="contact.php">Chính sách giải quyết khiếu nại</a></li>
+<li><a href="contact.php">Chính sách bảo mật thanh toán</a></li>
+</ul>
+<!--footer_ul_amrc ends here-->
+</div>
+
+
+<div class=" col-sm-4 col-md  col-6 col">
+<h5 class="headin5_amrc col_white_amrc pt2">TÀI KHOẢN CỦA TÔI</h5>
+<!--headin5_amrc-->
+<ul class="footer_ul_amrc">
+    <li><a href="login.php">Đăng kí</a></li>
+    <li><a href="cart.php">Xem giỏ hàng</a></li>
+    <li><a href="wishlist.php">Sản phẩm yêu thích</a></li>
+    <li><a href="profile.php">Chi tiết tài khoản</a></li>
+    <li><a href="contact.php">Liên hệ</a></li>
+</ul>
+<!--footer_ul_amrc ends here-->
+</div>
+
+
+<div class=" col-sm-4 col-md  col-12 col">
+<h5 class="headin5_amrc col_white_amrc pt2">Hỗ trợ</h5>
+<!--headin5_amrc ends here-->
+
+<ul class="footer_ul2_amrc">
+<p><i class="fa fa-phone"></i> Tổng đài hỗ trợ 0912345678  </p>
+<p><i class="fa fa fa-envelope"></i> info@watchstore.com  </p></ul>
+<!--footer_ul2_amrc ends here-->
+</div>
+</div>
+</div>
+
+
+<!-- <div class="container">
+<ul class="foote_bottom_ul_amrc">
+<li><a href="http://webenlance.com">Home</a></li>
+<li><a href="http://webenlance.com">About</a></li>
+<li><a href="http://webenlance.com">Services</a></li>
+<li><a href="http://webenlance.com">Pricing</a></li>
+<li><a href="http://webenlance.com">Blog</a></li>
+<li><a href="http://webenlance.com">Contact</a></li>
+</ul> -->
+<!--foote_bottom_ul_amrc ends here-->
+<p class="text-center">Copyright @2022 | Designed With by <a href="#">Watch Store</a></p>
+
+<ul class="social_footer_ul">
+<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+<li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+</ul>
+<!--social_footer_ul ends here-->
+</div>
+
+</footer>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
     <script type="text/javascript">
 		$(document).ready(function() {
 			/*
--- inc/header.php
+++ inc/header.php
@@ -36,6 +36,16 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
 <link href="css/menu.css" rel="stylesheet" type="text/css" media="all"/>
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+
+
 <script src="js/jquerymain.js"></script>
 <script src="js/script.js" type="text/javascript"></script>
 <script type="text/javascript" src="js/jquery-1.7.2.min.js"></script> 
